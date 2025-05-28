8a7b3bc6e8bbe853a962b8919b09ec950cf0b67f
Chocolate <sahrbidy@gmail.com>
2025-05-28 15:56:44 +0200

    Intégration  de TAnjombato

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -9,7 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class SearchController extends AbstractController
+class SearchController extends BaseController
 {
     #[Route('/search', name: 'app_search')]
     public function index(ObjectToRentRepository $objectToRentRepository): Response
