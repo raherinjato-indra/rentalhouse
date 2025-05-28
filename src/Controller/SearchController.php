42f8cec8c978808ccae8c25bcf7ffbb23a3008b0
Manoa <rabearisaoninamanoa@gmail.com>
2025-05-28 17:34:07 +0300

    INTEGRATION CITY vers QUARTIER - COORDONNEES - OBJECTTORENT

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -8,6 +8,7 @@ use App\Repository\ObjectToRentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\City;
 
 class SearchController extends BaseController
 {
@@ -21,5 +22,15 @@ class SearchController extends BaseController
             'objects' => $objects,
         ]);
     }
+    #[Route('/search/city/{id}', name: 'app_search_city')]
+    public function getCityById(City $city): Response
+      
+    {
+
+        return $this->render('search/searchCity.html.twig', [
+            'city' => $city
+        ]);
+    }
+   
 }
 
